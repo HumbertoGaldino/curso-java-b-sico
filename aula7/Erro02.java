9f757fd4999885581f16023e3006b14edbf5eb3a
Humberto Galdino <humberto.galdino@live.com>
2024-08-11 16:33:05 -0300

    Add aula7

---
+++ aula7/Erro02.java
@@ -0,0 +1,7 @@
+class Erro02 {
+    public static void main(String[] args) {
+        //erro de semântica
+        var int cont = 10;
+        System.out.println("Voce digitou: " + cont);
+    }
+}
---
+++ aula7/Erro03.java
@@ -0,0 +1,7 @@
+class Erro03 {
+    // erro em tempo de execução devido o método main não ter sido definido.
+    public static void Main(String[] args) {
+
+        System.out.println("Voce digitou: ");
+    }
+}
